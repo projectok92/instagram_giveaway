edc8c6aec2a62569ae1a148a355c9002cfce2ba4
Oleksandr Koval <lexkoval92@gmail.com>
2021-12-03 15:46:24 +0200

    feat: add 2/3 solution

--- App.js
+++ App.js
@@ -31,4 +31,23 @@ const uniqueValues = async () => {
   console.log('Unique: '+ uniqeNum);
 }
 
+const existInAllFiles = async () => {
+  const arrayWithArrays = await getPairsFromFiles();
+
+  let arr1;
+  let arr2;
+  let currentResult = arrayWithArrays[0];
+
+  for (let i = 1; i < 20; i++) {
+    arr1 = arrayWithArrays[i];
+    arr2 =  currentResult;
+
+    currentResult = arr1.filter(value => arr2.includes(value));
+    console.log(currentResult);
+  }
+
+  console.log('Exist in all files: ' + currentResult.length);
+}
+
 uniqueValues();
+existInAllFiles();
